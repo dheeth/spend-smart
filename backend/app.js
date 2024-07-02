e0bc51d3ab3b8a358d423bbea97d787ed0b36ce5
Pawan Kumar <39653409+dheeth@users.noreply.github.com>
2024-07-02 23:26:42 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -26,26 +26,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// CORS configurations (Allow specific origins)
-const allowedOrigins = ["*"]; // Add other trusted origins here
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Origin not allowed by CORS"));
-      }
-    },
-    credentials: true, // Include cookies
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  })
-);
-
 // Routes
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/incomes", authenticateUser, incomeRoutes);
